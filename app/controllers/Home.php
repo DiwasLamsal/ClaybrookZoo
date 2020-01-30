1135d56d926ed06c7d8f7a77d1b1f1dc38548a4b
Diwas Lamsal <diwaslamsal123@hotmail.com>
2020-01-30 17:04:42 +0545

    Commited from Group Project Project

---
+++ app/controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+
+  class Home extends Controller{
+
+    public function index(){
+      session_start();
+      if(isset($_SESSION['loggedin'])){
+        if($_SESSION['loggedin']['urole']=="Administrator")
+          header("Location:AdminHome");
+        else if($_SESSION['loggedin']['urole']=="Module Leader")
+          header("Location:ModuleLeaderHome");
+      }
+      else{
+        header("Location:home");
+      }
+
+    }
+
+  }
+
+
+
+
+
+?>
---
+++ app/index.php
@@ -0,0 +1,3 @@
+<?php
+  header("Location: /ZooAssignment/public/");
+ ?>
